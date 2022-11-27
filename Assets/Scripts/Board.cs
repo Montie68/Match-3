bb6586b760cc15b3b05698f70e7b05e64941f9f1
geoff <geoff@dropbeargames.com.au>
2022-11-27 23:40:01 +1100

    eod 20221127

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -16,9 +16,11 @@ public class Board : MonoBehaviour
 	[SerializeField] int _height = 8;
 	[SerializeField] int _borderSize;
 	[SerializeField] GameObject _tilePrefab;
+	[SerializeField] GameObject[] _gamePiecePrefabs;
 
 	Tile[,] _tiles;
-	
+	GamePiece[,] _gamePieces;
+
 	#endregion
 	#region DebugVariables
 	// debug variables go here
@@ -28,17 +30,19 @@ public class Board : MonoBehaviour
 	#region Unity Messages 
 	// Start is called before the first frame update
 	void Start()
-    {
+	{
 		_tiles = new Tile[_width, _height];
+		_gamePieces = new GamePiece[_width, _height];
 		SetupTiles();
 		SetUpCamera();
+		FillRandom();
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 	#endregion
 	#region Public Methods
 	//Place your public methods here
@@ -57,20 +61,59 @@ public class Board : MonoBehaviour
 				tile.transform.SetParent(transform);
 				tile.name = "Tile (" + i + ", " + j + ")";
 				_tiles[i, j] = tile.GetComponent<Tile>();
-				
+				_tiles[i, j].Init(i, j, this);
+
 			}
 		}
 	}
 
 	void SetUpCamera()
 	{
-		Camera.main.transform.position = new Vector3((float)(_width-1) / 2, (float)(_height-1) / 2, -10f);
+		Camera.main.transform.position = new Vector3((float)(_width - 1) / 2, (float)(_height - 1) / 2, -10f);
 
 		float aspectRatio = (float)Screen.width / (float)Screen.height;
-		float verticalSize = (float)_height / 2f + (float) _borderSize;
+		float verticalSize = (float)_height / 2f + (float)_borderSize;
 		float horizontalSize = ((float)_width / 2f + (float)_borderSize) / aspectRatio;
 
 		Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
 	}
+
+	GameObject GetRandomPiece()
+	{
+		int pieceIndex = Random.Range(0, _gamePiecePrefabs.Length); ;
+		while (_gamePiecePrefabs[pieceIndex] == null)
+		{
+			pieceIndex = Random.Range(0, _gamePiecePrefabs.Length);
+		}
+		return _gamePiecePrefabs[pieceIndex];
+	}
+
+	void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+	{
+		if (gamePiece == null)
+		{
+			Debug.LogWarningFormat("Game Piece prefab is null");
+			return;
+		}
+		gamePiece.transform.position = new Vector3(x, y, 0);
+		gamePiece.transform.rotation = Quaternion.identity;
+		gamePiece.SetCoord(x, y);
+
+	}
+
+	void FillRandom()
+    {
+		for (int i = 0; i < _width; i++)
+        {
+			for (int j = 0; j < _height; j++)
+            {
+				GameObject randomPiece = Instantiate(GetRandomPiece(), Vector3.zero, Quaternion.identity) as GameObject;
+				if (randomPiece != null)
+                {
+					PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+                }
+            }
+        }
+    }
 	#endregion
 }
